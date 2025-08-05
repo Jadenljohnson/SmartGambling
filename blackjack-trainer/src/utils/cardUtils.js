b7a9f55067756dbf8065e163be95012ac959ffae
Jadenljohnson <Jadenlaynejohnson@gmail.com>
2025-08-05 02:57:46 -0500

    random suit

--- blackjack-trainer/src/utils/cardUtils.js
+++ blackjack-trainer/src/utils/cardUtils.js
@@ -1,4 +1,11 @@
-export function getCardImageFileName(str_rank, str_suit = "clubs") {
+export function getCardImageFileName(str_rank, str_suit = null) {
+  const randomInt = Math.floor(Math.random()*4) + 1;
+  const suitMap = {
+    1: "clubs",
+    2: "diamonds",
+    3: "spades",
+    4: "hearts",
+  };
   const rankMap = {
     J: "jack",
     Q: "queen",
@@ -6,5 +13,6 @@ export function getCardImageFileName(str_rank, str_suit = "clubs") {
     A: "ace",
   };
   const str_convertedRank = rankMap[str_rank] || str_rank;
+  str_suit = suitMap[randomInt];
   return `${str_convertedRank}_of_${str_suit}.png`;
 }
